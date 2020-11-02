386c4e837a10a461300b4f1110244bd91c870eae
Derek Stobbs <dstobbs@googlemail.com>
2020-11-02 15:36:59 +0000

    Loony Lips: On-Screen Story
    - The test story is now displayed in the game window rather than in the console

--- Loony Lips/LoonyLips.cs
+++ Loony Lips/LoonyLips.cs
@@ -7,9 +7,7 @@ public class LoonyLips : Control
     {
         var prompts = new String[] { "Del", "ham", "mushroom", "tasty" };
         var story = "Once upon a time, {0} ate a {1} and {2} pizza, which was very {3}.";
-        GD.Print(String.Format(story, prompts));
 
-        prompts = new String[] { "Bob", "grape", "kiwi", "disgusting" };
-        GD.Print(String.Format(story, prompts));
+        GetNode<Label>("DisplayText").Text = String.Format(story, prompts);
     }
 }
