33a27fef4ee380354d2dba2630e7cddc68b152a7
Derek Stobbs <dstobbs@googlemail.com>
2020-11-02 16:33:23 +0000

    Loony Lips: Ok Button
    - When entering text, player can press the Ok button instead of pressing Enter

--- Loony Lips/LoonyLips.cs
+++ Loony Lips/LoonyLips.cs
@@ -9,7 +9,7 @@ public class LoonyLips : Control
         var story = "Once upon a time, {0} ate a {1} and {2} pizza, which was very {3}.";
 
         GetNode<Label>("VBoxContainer/DisplayText").Text = String.Format(story, prompts);
-        GetNode<LineEdit>("VBoxContainer/PlayerText").GrabFocus();
+        GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").GrabFocus();
     }
 
     public void OnPlayerTextTextEntered(String newText)
@@ -17,9 +17,15 @@ public class LoonyLips : Control
         UpdateDisplayText(newText);
     }
 
+    public void OnOkButtonPressed()
+    {
+        var newText = GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").Text;
+        UpdateDisplayText(newText);
+    }
+
     public void UpdateDisplayText(String newText)
     {
         GetNode<Label>("VBoxContainer/DisplayText").Text = newText;
-        GetNode<LineEdit>("VBoxContainer/PlayerText").Clear();
+        GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").Clear();
     }
 }
