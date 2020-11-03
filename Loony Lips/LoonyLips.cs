e26efc2aafa6991c2f5740cb67f15263984afcc6
Derek Stobbs <dstobbs@googlemail.com>
2020-11-03 16:10:23 +0000

    Loony Lips: Player Story
    - Player is now prompted to enter words, which are used at the end to tell the story

--- Loony Lips/LoonyLips.cs
+++ Loony Lips/LoonyLips.cs
@@ -1,31 +1,70 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class LoonyLips : Control
 {
+    List<String> _playerWords = new List<String>();
+    String _story = "Once upon a time, {0} ate a {1} and {2} pizza, which was very {3}.";
+    List<String> _prompts = new List<String>() { "a name", "a noun", "a noun", "an adverb" };
+
+    Label _displayText;
+    LineEdit _playerText;
+
+
     public override void _Ready()
     {
-        var prompts = new String[] { "Del", "ham", "mushroom", "tasty" };
-        var story = "Once upon a time, {0} ate a {1} and {2} pizza, which was very {3}.";
+        _displayText = GetNode<Label>("VBoxContainer/DisplayText");
+        _playerText = GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText");
+        _playerText.GrabFocus();
 
-        GetNode<Label>("VBoxContainer/DisplayText").Text = String.Format(story, prompts);
-        GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").GrabFocus();
+        _displayText.Text = "Welcome to Loony Lips\n";
+
+        CheckPlayerWordsLength();
     }
 
     public void OnPlayerTextTextEntered(String newText)
     {
-        UpdateDisplayText(newText);
+        AddToPlayerWords();
     }
 
     public void OnOkButtonPressed()
     {
-        var newText = GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").Text;
-        UpdateDisplayText(newText);
+        AddToPlayerWords();
     }
 
-    public void UpdateDisplayText(String newText)
+    public void AddToPlayerWords()
     {
-        GetNode<Label>("VBoxContainer/DisplayText").Text = newText;
-        GetNode<LineEdit>("VBoxContainer/HBoxContainer/PlayerText").Clear();
+        _playerWords.Add(_playerText.Text);
+        _displayText.Text = "";
+        _playerText.Clear();
+        CheckPlayerWordsLength();
+    }
+
+    public bool IsStoryDone()
+    {
+        return _playerWords.Count == _prompts.Count;
+    }
+
+    public void CheckPlayerWordsLength()
+    {
+        if (IsStoryDone())
+        {
+            TellStory();
+        }
+        else
+        {
+            PromptPlayer();
+        }
+    }
+
+    public void TellStory()
+    {
+        _displayText.Text = String.Format(_story, _playerWords.ToArray());
+    }
+
+    public void PromptPlayer()
+    {
+        _displayText.Text += "May I have " + _prompts[_playerWords.Count] + " please?";
     }
 }
