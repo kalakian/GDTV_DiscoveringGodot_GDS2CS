561eaada9670a1c2e3deaf53a73eb366e219283e
Derek Stobbs <dstobbs@googlemail.com>
2020-11-05 16:11:27 +0000

    Hoppy Days: Gravity & Jumping
    - Bunny falls when not on the ground
    - Bunny can jump when on the ground

--- Hoppy Days/Player/Player.cs
+++ Hoppy Days/Player/Player.cs
@@ -3,24 +3,50 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    Vector2 motion = new Vector2();
-    const float speed = 500;
+    Vector2 _motion = new Vector2();
+    const int _speed = 1000;
+    const int _gravity = 300;
+    const int _jumpSpeed = 3000;
 
     public override void _PhysicsProcess(float delta)
     {
-        motion.x = 0;
+        _motion.x = 0;
 
+        ApplyGravity();
+        Jump();
+        Move();
+        MoveAndSlide(_motion, Vector2.Up);
+    }
+
+    public void ApplyGravity()
+    {
+        if (!IsOnFloor())
+        {
+            _motion.y += _gravity;
+        }
+        else
+        {
+            _motion.y = 0;
+        }
+    }
+
+    public void Jump()
+    {
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+        {
+            _motion.y -= _jumpSpeed;
+        }
+    }
+
+    public void Move()
+    {
         if (Input.IsActionPressed("left"))
         {
-            motion.x -= speed;
+            _motion.x -= _speed;
         }
         if (Input.IsActionPressed("right"))
         {
-            motion.x += speed;
+            _motion.x += _speed;
         }
-
-        MoveAndSlide(motion);
-
-        base._PhysicsProcess(delta);
     }
 }
